5c51f97799ebce8498693f6798c87243dab9bfef
tibzzyyurng-creator <tibzzyyurng@gmail.com>
2026-03-10 10:33:23 +0100

    Version 1

--- salesApplicationAndGoodsRegistrationModule.c
+++ salesApplicationAndGoodsRegistrationModule.c
@@ -5,9 +5,9 @@
 #define MAX 20
 
    struct goods{
-   int id;
+   int id_of_good_available;
    char name[50];
-   float quantity_of_good;
+   int quantity_of_good;
    int unit_price;
 
    };
@@ -16,8 +16,10 @@
 { 
   struct goods g[MAX];
   int item, i;
-  int iD;
-  float quantity_of_good_requested;
+  int id_of_good_to_purchase;
+  int quantity_of_good_requested;
+  int x;
+  int current_goods;
   
   printf("Number of goods received? ");
   scanf("%d", &item );
@@ -31,13 +33,12 @@
   for (i = 0; i < item; i++){
       
       
-  int id = i + 1;
+   g[i].id_of_good_available = i + 1;
   
       
   printf("\nGood %d\n", i + 1);
       
-  printf("\nID of good = %d\n", i + 1);
-  //printf("%d", g[i].id);
+  printf("\nID of good = %d\n", g[i].id_of_good_available);
 
   printf("\nName of good - ");
   scanf("%s", g[i].name);
@@ -50,13 +51,13 @@
   else if(strlen(g[i].name) <= 50){
   
   printf("\nQuantity of good - ");
-  scanf("%f", &g[i].quantity_of_good);
-  printf("~Quantity of good is: %f\n",g[i].quantity_of_good);
+  scanf("%d", &g[i].quantity_of_good);
+  printf("~Quantity of good is: %d\n",g[i].quantity_of_good);
   
   printf("\nUnit price of goods - ");
   scanf("%d", &g[i].unit_price);
   printf("~Unit Price of good is: %d\n",g[i].unit_price);
-  g[i].id = id;
+ // g[i].id = id;
   
    printf("\n******************\n\n");
   }
@@ -65,35 +66,40 @@
   
   
    printf("\nID of good you want to buy? ");
-   scanf("%d", &iD );
+   scanf("%d", &id_of_good_to_purchase );
 
   //step 1 loop through array
 
-  for(int x = 0; x <  item; x++){ 
+  for( x = 0; x < item; x++){ 
 
-  struct goods current_good = g[x]; 
+    current_goods == x + 1;
 
-  //struct goods g[x];
-  if(iD == g[x].id){
+    g[x].id_of_good_available == current_goods;
+
+  if(current_goods == id_of_good_to_purchase){
     
        printf("\nHow many do you want to buy? ");
-       scanf("%f", &quantity_of_good_requested);
+       scanf("%d", &quantity_of_good_requested);
        
        if(quantity_of_good_requested > g[x].quantity_of_good){
-           printf("Insufficient availability of goods");
+           printf("\nInsufficient availability of goods");
            }
       
         else{
             printf("\nThe number of goods you requested is valid");
-            printf("\nThe number of %s remaining in stock = %f ",  g[x].name, g[x].quantity_of_good - quantity_of_good_requested );
+
+            printf("\nThe number of %s remaining in stock = %d ",  g[x].name, g[x].quantity_of_good - quantity_of_good_requested );
             
             }   
           }
-       
-       else{
-           printf("Invalid id... Input a valid id");
+
+
+            else{
+           printf("\nInvalid id... Input a valid id ...... Some informations below might be wrong!!!");
        } 
-      }                    
+        
+      }  
+      
       
       
     
@@ -108,19 +114,7 @@
 //       printf("\n<Good(s) listed above have been registered successfully..>\n");
        
        
-       
-          //     printf("\nID of good you want to buy? ");
-          //     scanf("%d", id );
-     
-     
-
-//     printf("\nHow many do you want to buy? ");
-//     scanf("%d", &qty1);
-//
-//     printf("\nWhat is the price of %s: ", item1);
-//     scanf("%f", &price1);
-
+   
   }
 	return 0;
 }
-
